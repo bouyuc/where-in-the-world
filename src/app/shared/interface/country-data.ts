d9a5c65abe4cf7427abdd4a47a2155a46b62f179
bouyuc <bouyuc@gmail.com>
2021-06-04 17:50:54 -0700

    country data interface

---
+++ src/app/shared/interface/country-data.ts
@@ -0,0 +1,15 @@
+export interface CountryData {
+    name: string,
+    population: string,
+    capital: string,
+    region: string,
+    flag: string,
+    nativeName: string,
+    topLevelDoamin: string[],
+    currencies: Object[],
+    languages: Object[],
+    subregion: string,
+    borders: string,
+    alpha2Code: string,
+    alpha3Code: string
+}
