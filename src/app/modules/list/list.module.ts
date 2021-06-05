84921002b2716d3994c8ec4392b65f6c60c60f4c
bouyuc <bouyuc@gmail.com>
2021-06-04 17:51:56 -0700

    country filter pipe

--- src/app/modules/list/list.module.ts
+++ src/app/modules/list/list.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list.component';
+import { CountryFilterPipe } from './country-filter.pipe';
 
 
 @NgModule({
   declarations: [
-    ListComponent
+    ListComponent,
+    CountryFilterPipe
   ],
   imports: [
     CommonModule,
